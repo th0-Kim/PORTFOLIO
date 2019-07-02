9b4b51452a6610c5f4e33200dec8ddfcd1b45f72
th0-Kim <ksyy3001@naver.com>
2019-07-02 22:38:04 +0900

    추가

--- js/common.js
+++ js/common.js
@@ -1,34 +1,13 @@
-$(document).ready(function(){
-	//SUB SCROLL MENU
-	var st = 0;
-	var $subScrollM = $(".header")
-	var $subScrollMTop = $(".header").offset().top;
+//SUB SCROLL MENU
+var st = 0;
+var $subScrollM = $(".header")
+var $subScrollMTop = $(".header").offset().top;
 
-	 $(window).scroll(function(e){
-		st = $(this).scrollTop();
-		if(st > $subScrollMTop){
-			$subScrollM.addClass("on");
-		}else{
-			 $subScrollM.removeClass("on");
-		}
-	 });
-
-
-	 //security
-
-	 //top
-	var $topScroll = $("footer") // 0~ 653
-	var $topScrollTop = $("footer").offset().bottom; //1011
-
-	 $(window).scroll(function(e){
-		st = $(this).scrollTop();
-
-		if(st > $topScrollTop){
-			$topScroll.removeClass("on");
-		}else{
-			$topScroll.addClass("on");
-		}
-		console.log($topScrollTop)
-	 });
-	 
-})
+	$(window).scroll(function(e){
+	st = $(this).scrollTop();
+	if(st > $subScrollMTop){
+		$subScrollM.addClass("on");
+	}else{
+			$subScrollM.removeClass("on");
+	}
+	});
