2610b6c7a7bc2b965eaa404f983cde7cfa2b33ee
th0Kim <3001th0@naver.com>
2020-02-15 21:58:57 +0900

    신규 메인 중간저장

--- inc/js/index.js
+++ inc/js/index.js
@@ -1,5 +1,11 @@
 /*
-// ver 0.0.2
+* Version   : ver 0.0.2
+* File      : js/index.js
+* Author    : KIM SO YOUNG
+* 
+* SUMMARY:
+* 1) SVG SCROLL EVENT - init, addEvent
+* 2) LIST EVENT - listInit, listEvent
 */
 function hasJqueryObject($elem) { return $elem.length > 0 }
 var app = app || {};
@@ -8,7 +14,7 @@ app = {
   init: function(item) {
     Wrap = item,
     bg = '.bg',
-    section = '.section',
+    section = 'section',
     mainSvg = '.mainSvg',
     text = 'text',
     scroll = 'scroll',
@@ -29,62 +35,54 @@ app = {
   addEvent: function() {
     $(window).on(scroll, function(){
       var winY = window.scrollY,
-          windowOW = window.outerWidth/3;
+          windowOW = window.outerWidth/3.5;
       if ( winY < 100 ) {
         TweenMax.set($mainSvg, {fontSize: 8 + em});
         TweenMax.set($text, {y:0});
       } else {
         if( winY > windowOW ) {
-            TweenMax.to($mainSvg, 0, {opacity:0});
+            TweenMax.to($mainSvg, 0, {opacity:0, zIndex:0});
             TweenMax.to($text, 0, {y:windowOW});
         } else {
-          TweenMax.to($mainSvg, 0, {opacity:1,fontSize: winY*3 + em});
+          TweenMax.to($mainSvg, 0, {opacity:1, zIndex:100,fontSize: winY*3 + em});
           TweenMax.to($text, 0, {y:winY});
         }
+      }
+
+      if( window.pageYOffset > $('.container').height() * 1.3 ) {
+        // TweenMax.to($bg, .2, {position:'absolute', opacity:0});
+        TweenMax.to($bg, .55, {y:'-450px', pacity:0});
+      } else {
+        // TweenMax.to($bg, .2, {position:'fixed', opacity:1});
+        TweenMax.to($bg, .55, {y:'0', opacity:1});
       }
     });
     
     let chText = document.querySelector('.mainSvg text');
     setTimeout(() => {
-      chText.innerHTML = "WEB PUBLISHER."
+      chText.innerHTML = "KIM SOYOUNG"
     }, 5000);
 
   },
-  headerScroll: function() {
-    var st = 0,
-    $subScrollM = $(".header"),
-    $subScrollMTop = $(".header").offset().top,
-    $topBtn = $(".top"),
-    $contentsTop = $(".contents").offset().top;
-
-    $(window).scroll(function(e){
-      //SUB SCROLL MENU
-      st = $(this).scrollTop();
-      if(st > $subScrollMTop){
-        $subScrollM.addClass("on");
-      }else{
-        $subScrollM.removeClass("on");
-      };
-
-      // TOP BUTTON
-      if ( st > $contentsTop ) {
-        $topBtn.fadeIn();
-      } else {
-        $topBtn.fadeOut();
-      };
-
-      // TOP BUTTON MOVE TO TOP
-      $topBtn.click( function() {
-        $( 'html, body' ).stop().animate( { scrollTop : 0 }, 400 );
-        return false;
-      } );
-    });
-    
+  listInit: function(){
+    this.item = '.listItem';
+    this.$item = app.$body.find(this.item);
+    app.listEvent();
+  },
+  listEvent: function(){
+      $(this.$item).on('mouseenter', function(){
+          TweenMax.to($(this).find('.btmArea'), .5, {height:'auto', opacity:1});
+          TweenMax.to($(this).find('.itemInfo'), .5, {top:'-15px', left:'-15px', padding:'280px 10px 10px'});
+      }).on('mouseleave', function(){
+          TweenMax.set($(this).find('.btmArea'), {height:0, opacity:0});
+          TweenMax.set($(this).find('.itemInfo'), {top:'15px', left:'15px', padding:'210px 10px 10px'});
+      })
   }
 }
   
 $(function(){
   app.$body = $("body");
   hasJqueryObject(app.$body.find(".wrap")) && app.init(".wrap");
+	hasJqueryObject(app.$body.find(".listItem")) && app.listInit();
   
 })
