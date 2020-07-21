92076e7970345eb513fbd587b6b4e4d687e5e494
th0-kim <ksyy3001@naver.com>
2020-07-21 17:02:05 +0900

    js 수정

--- inc/js/index.js
+++ inc/js/index.js
@@ -105,6 +105,6 @@ $(function(){
   app.$body = $("body");
   // hasJqueryObject(app.$body.find(".wrap")) && app.init(".wrap");
   hasJqueryObject(app.$body.find(".listItem")) && app.listInit(".listItem");
-  hasJqueryObject(app.$body.find(".cursor")) && app.cursror(".cursor");
+  // hasJqueryObject(app.$body.find(".cursor")) && app.cursror(".cursor");
   
 })
