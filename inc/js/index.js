b01e01ec1d0ae3daca65ce7c2806db9259c69eca
th0Kim <3001th0@naver.com>
2020-07-26 11:21:34 +0900

    최적화

--- inc/js/index.js
+++ inc/js/index.js
@@ -76,19 +76,22 @@ app = {
   },
   listInit: function(){
     this.item = '.listItem';
+    this.itemTit = '.itemTit';
     this.$item = app.$body.find(this.item);
     app.listEvent();
   },
   listEvent: function(){
       $(this.$item).on('mouseenter', function(){
+        var $itemTit = Number($(this).siblings(this.itemTit).height()) + 'px';
         $(this).addClass('on');
         TweenMax.to($(this).find('.btmArea'), .5, {height:'auto', opacity:1});
-        TweenMax.to($(this).find('.itemInfo'), .5, {top:'-15px', left:'-15px', padding:'280px 10px 10px'});
+        // console.log($itemTit + '10px 10px';
+        TweenMax.to($(this).find('.itemInfo'), .5, {top:'-15px', left:'-15px', paddingTop: $itemTit});
         TweenMax.to($('.cursor'), 0.5, {scale:0.8, ease:Power3.easeOut});
       }).on('mouseleave', function(){
         $(this).removeClass('on');
         TweenMax.set($(this).find('.btmArea'), {height:0, opacity:0});
-        TweenMax.set($(this).find('.itemInfo'), {top:'15px', left:'15px', padding:'210px 10px 10px'});
+        TweenMax.set($(this).find('.itemInfo'), {top:'15px', left:'15px', padding:'75% 10px 10px'});
         TweenMax.to($('.cursor'), 0.5, {scale:0.25, ease:Power3.easeOut});
       });
   },
